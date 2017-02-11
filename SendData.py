f8520d1ce7af518be51e85921e4a02c204aa2ec0
Alex Oxford <alex@oakhaven.org>
2017-02-11 03:12:04 -0500

    Added debug print

--- SendData.py
+++ SendData.py
@@ -25,7 +25,7 @@ class SendData(object):
 		data = self.get_last_row("/home/pi/Desktop/PSLT-Fullscale/Data/FlightData.csv")
 
 		if(not data == None):
-			#print data
+			print data
 			self.ser.write(data)
 
 	def End(self):
