d384b114fe843e73cac3ff2bd3f40cb63d013df4
Alex Oxford <alex@oakhaven.org>
2017-02-11 04:43:41 -0500

    Removed debug timers

--- RecordVideo.py
+++ RecordVideo.py
@@ -19,14 +19,10 @@ class RecordVideo(object):
 
 	def Update(self, picture=False, flush=False):
 		if(picture):
-			testStart = time.time()
 			self.camera.capture("/home/pi/Desktop/PSLT-Fullscale/Data/img.jpg", use_video_port=True)
-			print("Pic: " + str(time.time() - testStart))
 		if(flush):
-			testStart = time.time()
 			self.videoFile.flush()
 			os.fsync(self.videoFile.fileno())
-			print("Vid Flush: " + str(time.time() - testStart))
 		
 	def End(self):
 		self.camera.stop_recording()
